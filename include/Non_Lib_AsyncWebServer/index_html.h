9c37bc9715d2b0e798ab94f33fa55e321c98a9cd
Psylenceo_Work_PC <49788044+Psylenceo@users.noreply.github.com>
2023-04-16 21:22:15 -0400

    cleaned up safe home html

--- include/Non_Lib_AsyncWebServer/index_html.h
+++ include/Non_Lib_AsyncWebServer/index_html.h
@@ -180,53 +180,6 @@ const uint8_t index_html[] PROGMEM = R"rawliteral(
                         }, false);
                     }
 
-                    /*function loadStatus() {
-                        xmlhttp = new XMLHttpRequest();
-                        console.log("Hardware status request");
-                        xmlhttp.open("GET", "/hardwareStatus", false);
-                        xmlhttp.send();                        
-                    }*/
-                    
-                    source.addEventListener('hardware_status', function(e) {
-                        console.log("Hardware status response", e.data);
-                        document.getElementById("ARTICLE").innerHTML = e.data;
-                    }, false);
-
-                    source.addEventListener('SD_cardStatus', function(e) {
-                        console.log("SD_CARD status response", e.data);
-                        document.getElementById("SD_cardStatus").innerHTML = e.data;
-                    }, false);
-                    
-                    source.addEventListener('RX_Status', function(e) {
-                        console.log("Reciever status response", e.data);
-                        document.getElementById("RX_Status").innerHTML = e.data;
-                    }, false);
-                    
-                    source.addEventListener('IMU_Status', function(e) {
-                        console.log("IMU status response", e.data);
-                        document.getElementById("IMU_Status").innerHTML = e.data;
-                    }, false);
-                    
-                    source.addEventListener('GPS_Status', function(e) {
-                        console.log("GPS status response", e.data);
-                        document.getElementById("GPS_Status").innerHTML = e.data;
-                    }, false);
-
-                    source.addEventListener('Temp_Status', function(e) {
-                        console.log("Reciever status response", e.data);
-                        document.getElementById("Temp_Status").innerHTML = e.data;
-                    }, false);
-                    
-                    source.addEventListener('RPM_Status', function(e) {
-                        console.log("IMU status response", e.data);
-                        document.getElementById("RPM_Status").innerHTML = e.data;
-                    }, false);
-                    
-                    source.addEventListener('Battery_Status', function(e) {
-                        console.log("GPS status response", e.data);
-                        document.getElementById("Battery_Status").innerHTML = e.data;
-                    }, false);
-                    
                     source.addEventListener('Webpage_Upload', function(e) {
                         console.log("Webpage_Upload response", e.data);
                         document.getElementById("ARTICLE").innerHTML = e.data;
@@ -349,8 +302,6 @@ const uint8_t index_html[] PROGMEM = R"rawliteral(
                             window.open(urltocall, "_blank");
                          }
                     }
-
-                    /*setTimeout(loadStatus,500);*/
                 </script>
             </article>
         </section>
