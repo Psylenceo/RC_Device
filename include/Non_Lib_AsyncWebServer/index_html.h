73a77ecc5eaf6fae9689e7995a7b728f29becc84
Psylenceo_Work_PC <49788044+Psylenceo@users.noreply.github.com>
2023-05-01 23:44:04 -0400

    slimming down and only needed for file upload

--- include/Non_Lib_AsyncWebServer/index_html.h
+++ include/Non_Lib_AsyncWebServer/index_html.h
@@ -3,155 +3,27 @@
 
 
 const uint8_t index_html[] PROGMEM = R"rawliteral(
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>RC Light Controller</title>
-        <link rel="icon" type="image/png" href="https://www.arrmaforum.com/favicon-32x32.png" sizes="32x32" />
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <style>
-            * {
-             box-sizing: border-box;
-            }
-
-            body {
-                font-family: Arial, Helvetica, sans-serif;
-            }
-
-            /* Style the header */
-            header {
-                background-color: #666;
-                padding: 30px;
-                text-align: center;
-                font-size: 35px;
-                color: white;
-            }       
-
-            /* Container for flexboxes */
-            section {
-                display: -webkit-flex;
-                display: flex;
-            }
-
-            /* Style the navigation menu */
-            nav {
-                -webkit-flex: 1;
-                -ms-flex: 1;
-                flex: 1;
-                background: #ccc;
-                padding: 20px;
-                text-align: Left
-            }
-
-            /* Style the list inside the menu */
-            nav ul {
-                list-style-type: none;
-                padding: 0;
-            }
-
-            button.Sidebar { background:none; border:none; }
-
-            .Sidebar:hover {
-                background:transparent;
-                cursor: pointer;
-            }
-
-            /* Style the content */
-            article {
-                -webkit-flex: 3;
-                -ms-flex: 3;
-                flex: 3;
-                background-color: #f1f1f1;
-                padding: 10px;
-                text-align: center
-            }
-
-            .switch {
-                position: relative;
-                display: inline-block;
-                width: 60px;
-                height: 34px
-            }
-
-            .switch input {
-                display: none
-            }
-
-            .slider {
-                position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                bottom: 0;
-                background-color: #ccc;
-                border-radius: 17px
-            }
-
-            .slider:before {
-                position: absolute;
-                content: "";
-                height: 26px;
-                width: 26px;
-                left: 4px;
-                bottom: 4px;
-                background-color: #fff;
-                -webkit-transition: .4s;
-                transition: .4s;
-                border-radius: 34px
-            }
-            .flex-container {
-            display: flex;
-            }
-            .flex-container > div {
-                display: flex;
-            }
-
-            input:checked+.slider {
-                background-color: #2196F3
-            }
-
-            input:checked+.slider:before {
-                -webkit-transform: translateX(26px);
-                -ms-transform: translateX(26px);
-                transform: translateX(26px)
-            }
-
-            /* Style the footer */
-            footer {
-                background-color: #777;
-                padding: 10px;
-                text-align: center;
-                color: white;
-            }
-
-            /* Responsive layout - makes the menu and the content (inside the section) sit on top of each other instead of next to each other */
-            @media (max-width: 600px) {
-                section {
-                    -webkit-flex-direction: column;
-                    flex-direction: column;
-                }
-            }
-        </style>
     </head>
     <body>
-        <header>
-            <h1>RC Light Controller</h1>
-            <h3>SafeMode</h3>
-        </header>
-        <section>
-            <nav>
-                <ul>
-                    %SIDEBAR%
-                    <li><a href="/SafeMode"> SafeMode</a></li>
-				    <li><a href="/update"> OTA Update</a></li>
-                    <button class = "Sidebar", id = "Webpage_Upload" onclick="webpageRequest(this)"><u>Webpage Upload</u></button>
-                </ul>
-            </nav>
-            <article>
-                <div id="ARTICLE"></div>
-                <script>
+        <h1>Web Page File Management</h1>
+            <p>SD Card Size: <span id="SD_size"></span></p>
+            <p>SD Card used: <span id="SD_used"></span></p>
+            <p>SD Card Free: <span id="SD_free"></span></p>
+            <p>
+                <button onclick="listFilesButton()">List Files</button>
+                <button onclick="showUploadButtonFancy()">Upload File</button>
+            </p>
+            <p id="status"></p>
+            <p id="detailsheader"></p>
+            <p id="details"></p>
+
+            <script>
                     var dt = new Date();
                     setTimeout(console.log(dt.toString()), 100);
                     document.addEventListener("DOMContentLoaded", function () {
@@ -182,36 +54,6 @@ const uint8_t index_html[] PROGMEM = R"rawliteral(
                             }
                         }, false);
                     }
-
-                    source.addEventListener('Webpage_Upload', function(e) {
-                        console.log("Webpage_Upload response", e.data);
-                        document.getElementById("ARTICLE").innerHTML = e.data;
-                    }, false);
-
-                    function webpageRequest(element){
-                        var xhr = new XMLHttpRequest();
-                        xhr.open("Get", "/"+element.id, true);
-                        console.log(element.id+" request");
-                        xhr.send();
-                        if(element.id == "HardwareConfig"){
-                            document.getElementById("ARTICLE").innerHTML = Response.text();
-                        }
-                    }
-
-                    source.addEventListener('SD_size', function (e) {
-                        console.log("SD_size", e.data);
-                        document.getElementById("SD_size").innerHTML = e.data + " MB";
-                    }, false)
-
-                    source.addEventListener('SD_used', function (e) {
-                        console.log("SD_used", e.data);
-                        document.getElementById("SD_used").innerHTML = e.data + " KB";
-                    }, false)
-
-                    source.addEventListener('SD_free', function (e) {
-                        console.log("SD_free", e.data);
-                        document.getElementById("SD_free").innerHTML = e.data + " MB";
-                    }, false)
 
                     function listFilesButton() {
                         xmlhttp = new XMLHttpRequest();
@@ -306,38 +148,9 @@ const uint8_t index_html[] PROGMEM = R"rawliteral(
                          }
                     }
                 </script>
-            </article>
-        </section>
-        <footer>
-            <p>,<span ID ="RTC"></span></p>
-        </footer>
+        <p>,<span ID ="RTC"></span></p>
      </body>
 </html>   
 )rawliteral";
-const char w_upload[] PROGMEM = R"rawliteral(
-    <h1>Web Page File Management</h1>
-            <p>SD Card Size: <span id="SD_size"></span></p>
-            <p>SD Card used: <span id="SD_used"></span></p>
-            <p>SD Card Free: <span id="SD_free"></span></p>
-            <p>
-                <button onclick="listFilesButton()">List Files</button>
-                <button onclick="showUploadButtonFancy()">Upload File</button>
-            </p>
-            <p id="status"></p>
-            <p id="detailsheader"></p>
-            <p id="details"></p>
-)rawliteral";
-const char status[] PROGMEM = R"rawliteral(
-    <h1>Connected devices</h1>
-            <p>SD Card: <span id="SD_cardStatus"></span></p>
-            <p>Reciever: <span id="RX_Status"></span></p>
-            <p>IMU: <span id="IMU_Status"></span></p>
-            <p>GPS: <span id="GPS_Status"></span></p>
-            <p>Temperature sensor(s): <span id="Temp_Status"></span></p>
-            <p>RPM Sensor: <span id="RPM_Status"></span></p>
-            <p>Battery Pack: <span id="Battery_Status"></span></p>
-            
-)rawliteral";
-
 
 #endif
